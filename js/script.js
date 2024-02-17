618307678767f0f292752896a0ccda4183c696b6
animes barman <animesbarman101@gmail.com>
2024-02-17 13:03:31 +0600

    projectr is done

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 function play(){
 
-    hidden('home-screen')
+    hidden('home-screen');
+    hidden('final-score')
     show('play')
+    setTextById('currentLife',5);
+    setTextById('currentScore',0)
     continewGame()
 
 
 };
+
+
 
 function hidden(getId){
 
@@ -55,4 +60,60 @@ function addBackgroundColor(getId){
     setTimeout(()=>{
         id.classList.remove('bg-purple-800')
     },2000)
+}
+
+function getTextById(getId){
+    const getElementById=document.getElementById(getId);
+    const text=getElementById.innerText;
+    const score=parseInt(text);
+    return score;
+}
+function setTextById(getId,value){
+
+    const text=document.getElementById(getId);
+    text.innerText=value;
+
+}
+
+document.addEventListener('keyup',function(e){
+    const playerKeyPress=e.key;
+
+    if(playerKeyPress === 'Backspace'){
+        overTheGame();
+    }
+
+
+    const currentAlphabet=document.getElementById('currentAlphabet').innerText;
+    const expetAlphabet=currentAlphabet.toLocaleLowerCase();
+
+    if(playerKeyPress === expetAlphabet){
+
+        const currentScore=getTextById('currentScore')
+        const newScore=currentScore + 1;
+        setTextById('currentScore',newScore);
+
+        continewGame()
+    }else{
+        const lineCureent=getTextById('currentLife');
+
+        const gameOver=lineCureent -1;
+        setTextById('currentLife',gameOver);
+
+        if(gameOver === 0){
+
+            overTheGame();
+
+    
+        }
+    }
+})
+
+
+function overTheGame(){
+    hidden('play');
+    show('final-score')
+
+    const lastScore=document.getElementById('currentScore');
+    console.log(lastScore.innerText);
+    setTextById('game-score' , lastScore.innerText)
 }
